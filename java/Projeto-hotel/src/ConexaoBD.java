ff4e19bd081477e5b4254af0e446106d008380a0
cc23632 <cc23632@g.unicamp.br>
2023-10-30 22:41:34 -0300

    Create ConexaoBD.java

---
+++ java/Projeto-hotel/src/ConexaoBD.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD{
+    private static final String URL = "jdbc:sqlserver://REGULUS:1433;databaseNome=BD23632";
+    private static final String USUARIO = "BD23632";
+    private static final String SENHA = "BD23632";
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            System.out.println("Conexão bem-sucedida com o banco de dados.");
+            return conexao;
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Falha na conexão com o banco de dados: [ERRO] " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
