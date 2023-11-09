e21738e6ed152afa92cd7efad31b3df7eb400b1a
cc23632 <cc23632@g.unicamp.br>
2023-11-08 22:47:33 -0300

    Update ConexaoBD.java

--- java/Projeto-hotel/src/ConexaoBD.java
+++ java/Projeto-hotel/src/ConexaoBD.java
@@ -1,24 +1,31 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ConexaoBD{
-    private static final String URL = "jdbc:sqlserver://regulus.cotuca.unicamp.br:1433;databaseNome=BD23632; user=BD23632; password=BD23632; integratedSecurity=true; encrypt=true; trustServerCertificate=true; authenticationScheme=NTLM"; //
-    private static final String USUARIO = "BD23632";
-    private static final String SENHA = "BD23632";
+    private static final String URL = "jdbc:sqlserver://regulus.cotuca.unicamp.br:1433; databaseNome=BD23632; user=BD23632; password=BD23632; encrypt=true; trustServerCertificate=true;";
 
     public static Connection conectar() {
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
             Connection conexao = DriverManager.getConnection(URL);
             System.out.println("Conexão bem-sucedida com o banco de dados.");
             return conexao;
 
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             System.err.println("Falha na conexão com o banco de dados: [ERRO] " + e.getMessage());
             e.printStackTrace();
             return null;
         }
+    }
+
+    public void select(){
+        String select = "select * from hotel.TipoQuarto";
+        try{
+            Statement statement = conexao.createStatement();
+            resultSet = statement.executeQuery(select);
+        }
+
     }
 }
