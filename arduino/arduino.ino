bb94496161d6985de2a5cca27aa9adb6580ab578
cc23632 <cc23632@g.unicamp.br>
2023-11-29 14:27:23 -0300

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,4 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //Inclusão da Biblioteca
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
 
 const int vermelho = 6;
@@ -20,26 +20,34 @@ void loop(){
     entrada = Serial.read();
     Serial.print("Recebido: ");
     Serial.println(entrada);
-
-    if(entrada == 'A' || entrada == 'a'){
-      digitalWrite(vermelho, LOW);
-      digitalWrite(backLight, HIGH);
-      lcd.clear();
-  	  lcd.setCursor(0, 0);
-      lcd.print("Reserva Feita");
-      lcd.setCursor(0, 1);
-      lcd.print("com Sucesso");
-      digitalWrite(verde, HIGH);
-      
-    }else if(entrada == 'B' || entrada == 'b'){
-      digitalWrite(verde, LOW);
-      digitalWrite(backLight, HIGH);
-      lcd.clear();
-  	  lcd.setCursor(0, 0);
-      lcd.print("Erro em fazer");
-      lcd.setCursor(0, 1);
-      lcd.print("a reserva");
-      digitalWrite(vermelho, HIGH);
+    
+    switch(entrada){
+      case '1':
+      	digitalWrite(vermelho, LOW);
+        digitalWrite(backLight, HIGH);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Removido com");
+        lcd.setCursor(0, 1);
+        lcd.print("Sucesso");
+        digitalWrite(verde, HIGH);
+      	break;
+      case '2':
+      	digitalWrite(verde, LOW);
+        digitalWrite(backLight, HIGH);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("[ERRO]Falha ao");
+        lcd.setCursor(0, 1);
+        lcd.print("remover");
+        digitalWrite(vermelho, HIGH);
+      	break;
+      default:
+      	digitalWrite(vermelho, LOW);
+      	digitalWrite(verde, LOW);
+      	lcd.clear();
+      	Serial.print("não da");
     }
+
   }
 }
