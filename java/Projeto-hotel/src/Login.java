0f606b20a11c4530baa6b7c838e626d83ff4eee0
cc23632 <cc23632@g.unicamp.br>
2023-11-29 22:18:59 -0300

    Update Login.java

--- java/Projeto-hotel/src/Login.java
+++ java/Projeto-hotel/src/Login.java
@@ -80,9 +80,11 @@ public class Login extends JFrame{
         }
     }
 
+    /*
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new Login();
         });
     }
+    */
 }
