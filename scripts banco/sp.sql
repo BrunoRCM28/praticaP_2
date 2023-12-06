0ba5683f36897d9ac93d2a331b21e23bbf9a7ef6
cc23537 <cc23537@g.unicamp.br>
2023-12-05 23:22:32 -0300

    Update sp.sql

--- scripts banco/sp.sql
+++ scripts banco/sp.sql
@@ -11,6 +11,38 @@ create or alter procedure hotel.deletaesta
 @id int
 as begin
 	begin
-	delete from hotel.Estacionamento where id_Vaga = @i
+	delete from hotel.Estacionamento where id_Vaga = @id
+	end
+end
+
+create or alter procedure hotel.deletahos
+@id int
+as begin
+	begin
+	delete from hotel.Hospedes where codigoCliente = @id
+	end
+end
+
+create or alter procedure hotel.deletaquarto
+@id int
+as begin
+	begin
+	delete from hotel.Quarto where numeroQuarto = @id
+	end
+end
+
+create or alter procedure hotel.deletatipo
+@id int 
+as begin
+	begin
+	delete from hotel.TipoQuarto where id_Tipo = @id
+	end
+end
+
+create or alter procedure hotel.inserehos
+@nome varchar(25),@sobrenome varchar(25),@cpf bigint,@telefone bigint,@email varchar(50),@rg bigint, @sexo char(1)
+as begin
+	begin
+		insert into hotel.Hospedes(prenome,sobrenome,cpf,telefone,e_mail,rg,sexo) values (@nome,@sobrenome,@cpf,@telefone,@email,@rg,@sexo)
 	end
 end
